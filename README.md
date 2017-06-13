1742b5b0cc43d132d1e06711c15ff10195319ad2
adoniajagan <bjagan@ventechsolutions.com>
2017-06-13 13:39:05 +0530

    All files add

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Gmibot" 
+"# Gmibot" 
---
+++ messages/config.js
@@ -0,0 +1,16 @@
+var config = {}
+
+config.endpoint = "https://gmi.documents.azure.com:443/";
+config.primaryKey = "WIuuor9qdc8S2gAY1RzTDgUZcAHEKoRvoLq3yKDCMKq3HJ4bYxYQfF1lzchsllmECoGsD744frwsdHGCQlMIlQ==";
+
+// ADD THIS PART TO YOUR CODE
+config.database = {
+    "id": "gmi"
+};
+
+config.collection = {
+    "id": "mygmi"
+};
+
+
+module.exports = config;
